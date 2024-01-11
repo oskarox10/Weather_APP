e54ec12e60a359b95e020bca09dede29424e0f26
Oskar <oskarox10@gmail.com>
2024-01-11 13:32:00 +0100

    dodanie metoedy do repository, ktora powinna pobierac informacje z Geo Api i dawac koordynat

--- src/main/java/com/example/demo/GeoCodingRepository.java
+++ src/main/java/com/example/demo/GeoCodingRepository.java
@@ -1,17 +1,51 @@
 package com.example.demo;
 
 
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.asm.TypeReference;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
+import org.springframework.web.client.RestTemplate;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Optional;
 
 @Repository
 public class GeoCodingRepository {
 
     private final String GeoCodingURL = "http://api.openweathermap.org/geo/1.0/direct?q=";
 
-    public String findCoordinatesByCity(String City){
+
+    private ObjectMapper objectMapper;
+    private RestTemplate restTemplate;
+
+    @Autowired
+    private GeoCodingRepository(RestTemplate restTemplate, ObjectMapper objectMapper){
+        this.restTemplate = restTemplate;
+        this.objectMapper=objectMapper;
+    }
+
+
+    public JsonNode findCoordinatesByCity(String City) throws IOException {
+        JsonNode json;
+
+        InputStream inputStream = getClass().getClassLoader().getResourceAsStream(GeoCodingURL + City + "&limit=5&appid=fc020e00e72e06c98a1095659064252b");
+        json = objectMapper.readValue(inputStream, JsonNode.class);
+
+        JsonNode lat = getLat(json);
+
+        return lat;
 
     }
 
+    private JsonNode getLat(JsonNode json) {
+        return Optional.ofNullable(json)
+                .map(jsonNode -> jsonNode.get("lon"))
+                .orElseThrow(() -> new RuntimeException("Invalid Jon"));
+
+    }
 
 
 }
