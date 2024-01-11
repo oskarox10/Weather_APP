6b162d202ba2c4fe29fe4b3be02edd5aae4a7213
Oskar <oskarox10@gmail.com>
2024-01-11 12:57:04 +0100

    dodanie do klasy config, RestTemplete w celu odblokowania dostepu do wykonywania zapytan HTTP

--- src/main/java/com/example/demo/WebConfig.java
+++ src/main/java/com/example/demo/WebConfig.java
@@ -1,4 +1,17 @@
 package com.example.demo;
 
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestTemplate;
+
+@Configuration
 public class WebConfig {
+
+    @Bean
+    public RestTemplate restTemplate(RestTemplateBuilder builder){
+        return builder.build();
+    }
+
+
 }
